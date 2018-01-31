fa14e2f7e704f2b8baa84e58abd05af56e98ebc9
panshiqu <panshiqu@sina.com>
2018-01-31 15:10:49 +0800

    move file

--- spider/spider.go
+++ spider/spider.go
@@ -1,12 +1,20 @@
-package main
+package spider
+
+import (
+	"github.com/panshiqu/framework/utils"
+)
 
 // Spider 蜘蛛
 type Spider struct {
+	schedule *utils.Schedule
 }
 
 // NewSpider 创建
 func NewSpider() *Spider {
-	return nil
+	s := &Spider{}
+	s.schedule = utils.NewSchedule(s)
+	go s.schedule.Start()
+	return s
 }
 
 // OnTimer 定时器到期
