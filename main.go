737ecb6d33f45e473bf1e524ad7c31cc70f26807
panshiqu <panshiqu@sina.com>
2018-02-01 16:08:36 +0800

    display content

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/panshiqu/shopping/cache"
+	"github.com/panshiqu/shopping/db"
 	"github.com/panshiqu/shopping/spider"
 )
 
@@ -14,7 +17,35 @@ var index = template.Must(template.New("index").Parse(`<html><body><table>
 	</table></body></html>`))
 
 func procRequest(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Ins.Query("SELECT sku FROM sku ORDER BY priority")
+	if err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
 
+	defer rows.Close()
+
+	var ids []int64
+	for rows.Next() {
+		var sku int64
+
+		if err := rows.Scan(sku); err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+
+		ids = append(ids, sku)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
+
+	if err := index.Execute(w, cache.Select(ids)); err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
 }
 
 func main() {
